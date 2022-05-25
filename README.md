07888bff8aba5dbf815fe969d0a93c69308f0f1c
Bruno-55 <brunosnmuniz@gmail.com>
2022-05-25 18:41:37 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Loteria
+
+## This program is made for *_personal_* use only
+
+### This program guesses random numbers to be gambled on specific games in the brasilian lottery.
---
+++ quina.py
@@ -0,0 +1,19 @@
+"""
+	Esse programa vai tentar adivinhar os numeros da quina.
+"""
+from random import randint
+
+
+jogo = []
+
+# Esta funcao vai continuar apresentando numeros aleatorios ate 15 dezenas nao repetidas popularem a lista "jogo"
+while len(jogo) < 5:
+	dezena = randint(1, 80)
+	if dezena in jogo:
+		pass
+	else:
+		jogo.append(dezena)
+
+jogo.sort()
+
+print(jogo)
